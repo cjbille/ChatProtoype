d1f6040b47a07b303178244e4a066f595133def2
Chris Bille <cbille1@icloud.com>
2024-09-18 20:38:16 -0400

    Initial Commit

---
+++ ChatProtoype/ChatProtoypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatProtoypeApp.swift
+//  ChatProtoype
+//
+//  Created by Christopher Bille on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatProtoypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatProtoypeTests/ChatProtoypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatProtoypeTests.swift
+//  ChatProtoypeTests
+//
+//  Created by Christopher Bille on 9/18/24.
+//
+
+import Testing
+@testable import ChatProtoype
+
+struct ChatProtoypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
